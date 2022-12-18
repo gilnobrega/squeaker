8d745eaf62ba67420462c2218270accd1c7ce30f
Gil Nobrega <contact@gilnobre.ga>
2022-12-18 23:31:17 +0000

    improves RotaryPageView does not vibrate if outside scrolling bounds

--- lib/widgets/rotary_page_view.dart
+++ lib/widgets/rotary_page_view.dart
@@ -7,9 +7,22 @@ import 'package:wearable_rotary/wearable_rotary.dart';
 class RotaryPageView extends StatefulWidget {
   final List<Widget> children;
   final RotaryScrollController? rotaryScrollController;
+  final PageController? pageController;
+  final Duration duration;
+  final Curve curve;
+  final ScrollPhysics physics;
+  final Axis scrollDirection;
 
-  const RotaryPageView(
-      {super.key, required this.children, this.rotaryScrollController});
+  const RotaryPageView({
+    super.key,
+    required this.children,
+    this.rotaryScrollController,
+    this.pageController,
+    this.duration = const Duration(milliseconds: 250),
+    this.curve = Curves.easeInOutCirc,
+    this.physics = const NeverScrollableScrollPhysics(),
+    this.scrollDirection = Axis.horizontal,
+  });
 
   @override
   State<RotaryPageView> createState() => _RotaryPageViewState();
@@ -17,32 +30,46 @@ class RotaryPageView extends StatefulWidget {
 
 class _RotaryPageViewState extends State<RotaryPageView> {
   late final RotaryScrollController _rotaryScrollController;
-  final _pageController = PageController();
+  late final PageController _pageController;
 
   static const _kVibrationDuration = 25;
   static const _kVibrationAmplitude = 64;
 
-  static const _kPageSwitchAnimationDuration = Duration(milliseconds: 150);
-  static const _kPageSwitchAnimationCurve = Curves.easeInOut;
-
   late final StreamSubscription<RotaryEvent> _rotarySubscription;
 
-  Future<void> _scrollToPage(int addPage) {
+  late int _currentPage;
+
+  Future<void> _scrollToPage(int page) {
     return _pageController.animateToPage(
-      _pageController.page!.toInt() + addPage,
-      duration: _kPageSwitchAnimationDuration,
-      curve: _kPageSwitchAnimationCurve,
+      page,
+      duration: widget.duration,
+      curve: widget.curve,
     );
   }
 
+  bool _isAtEdge(RotaryDirection direction) {
+    switch (direction) {
+      case RotaryDirection.clockwise:
+        return _currentPage == widget.children.length - 1;
+      case RotaryDirection.counterClockwise:
+        return _currentPage == 0;
+    }
+  }
+
   void _rotaryEventListener(RotaryEvent event) {
-    _scrollToPage(event.direction == RotaryDirection.clockwise ? 1 : -1);
+    if (_isAtEdge(event.direction)) return;
+    _currentPage += (event.direction == RotaryDirection.clockwise ? 1 : -1);
+    _scrollToPage(_currentPage);
     Vibration.vibrate(
-        duration: _kVibrationDuration, amplitude: _kVibrationAmplitude);
+      duration: _kVibrationDuration,
+      amplitude: _kVibrationAmplitude,
+    );
   }
 
   @override
   void initState() {
+    _pageController = widget.pageController ?? PageController();
+    _currentPage = _pageController.initialPage;
     _rotaryScrollController = widget.rotaryScrollController ??
         RotaryScrollController(maxIncrement: 50);
     _rotarySubscription = rotaryEvents.listen(_rotaryEventListener);
@@ -61,8 +88,8 @@ class _RotaryPageViewState extends State<RotaryPageView> {
   Widget build(BuildContext context) {
     return PageView(
       controller: _pageController,
-      scrollDirection: Axis.horizontal,
-      physics: const NeverScrollableScrollPhysics(),
+      scrollDirection: widget.scrollDirection,
+      physics: widget.physics,
       children: widget.children,
     );
   }
